6c8a35ccd9de4108a00260860f10444d90176b00
leonshiyan <leonshiyan@hotmail.com>
2023-04-11 13:58:22 +0800

    Add TreeNode definition

--- src/SortedListToBST.java
+++ src/SortedListToBST.java
@@ -1,3 +1,26 @@
+import java.util.Arrays;
+import java.util.Random;
+
+/**
+ * Definition for a binary tree node.
+ */
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {}
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
 
 public class SortedListToBST {
 

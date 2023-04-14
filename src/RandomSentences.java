e1b2bf6af298d74adc9e149e147fb42ee699e8b0
leonshiyan <leonshiyan@gmail.com>
2023-04-14 12:00:33 +0800

    add arrays and random obj

--- src/RandomSentences.java
+++ src/RandomSentences.java
@@ -1,6 +1,18 @@
+import java.util.Random;
 
 public class RandomSentences {
+	 // Declare arrays of strings representing various parts of speech
+    static final String[] properNouns = {"Fred", "Jane", "Richard Nixon", "Miss America"};
+    static final String[] commonNouns = {"man", "woman", "fish", "elephant", "unicorn"};
+    static final String[] determiners = {"a", "the", "every", "some"};
+    static final String[] adjectives = {"big", "tiny", "pretty", "bald"};
+    static final String[] intransitiveVerbs = {"runs", "jumps", "talks", "sleeps"};
+    static final String[] transitiveVerbs = {"loves", "hates", "sees", "knows", "looks for", "finds"};
+    static final String[] conjunctions = {"and", "or", "but", "because"};
 
+    // Declare a Random object for generating random numbers
+    static final Random random = new Random();
+    
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 

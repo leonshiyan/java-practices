2a00048bfb8ebe4f12a9a250a8a488ee15b4c768
leonshiyan <leonshiyan@gmail.com>
2023-04-18 14:29:29 +0800

    add javadoc

--- src/RecursiveFunctions.java
+++ src/RecursiveFunctions.java
@@ -1,6 +1,13 @@
-
+/**
+ * A class that contains two recursive functions to compute factorial and Fibonacci.
+ */
 public class RecursiveFunctions {
-
+	
+	/**
+	 * Tests the factorial and fibonacci functions.
+	 * 
+	 * @param args Unused.
+	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 

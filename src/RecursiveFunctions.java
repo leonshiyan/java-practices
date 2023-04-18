d829e1a2597dc60cd94bc9cff7d5dbce7313cfb4
leonshiyan <leonshiyan@gmail.com>
2023-04-18 14:36:39 +0800

    add fib and fab functions

--- src/RecursiveFunctions.java
+++ src/RecursiveFunctions.java
@@ -2,6 +2,32 @@
  * A class that contains two recursive functions to compute factorial and Fibonacci.
  */
 public class RecursiveFunctions {
+	/**
+	 * Returns the factorial of the given non-negative integer using recursion.
+	 * 
+	 * @param n The non-negative integer to compute the factorial of.
+	 * @return The factorial of n.
+	 */
+	public static int factorial(int n) {
+		if (n == 0) {
+	      return 1;
+	    } else {
+	      return n * factorial(n - 1);
+	    }
+	}
+	/**
+	 * Returns the nth Fibonacci number using recursion.
+	 * 
+	 * @param n The non-negative integer representing the nth Fibonacci number to compute.
+	 * @return The nth Fibonacci number.
+	 */
+	public static int fibonacci(int n) {
+	    if (n == 0 || n == 1) {
+	      return 1;
+	    } else {
+	      return fibonacci(n - 1) + fibonacci(n - 2);
+	    }
+	 }
 	
 	/**
 	 * Tests the factorial and fibonacci functions.

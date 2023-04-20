dce9fe7721b9ea7535d9ca68e720f148815dc2e1
leonshiyan <leonshiyan@gmail.com>
2023-04-20 15:47:28 +0800

    add move right method

--- src/turing/Tape.java
+++ src/turing/Tape.java
@@ -7,15 +7,19 @@ public class Tape {
         currentCell = new Cell();
         currentCell.content = ' ';
     }
+    
     public Cell getCurrentCell() {
         return currentCell;
     }
+    
     public char getContent() {
         return currentCell.content;
     }
+    
     public void setContent(char ch) {
         currentCell.content = ch;
     }
+    
     public void moveLeft() {
         if (currentCell.prev == null) {
             Cell newCell = new Cell();
@@ -24,5 +28,15 @@ public class Tape {
             currentCell.prev = newCell;
         }
         currentCell = currentCell.prev;
+    }
+    
+    public void moveRight() {
+        if (currentCell.next == null) {
+            Cell newCell = new Cell();
+            newCell.content = ' ';
+            newCell.prev = currentCell;
+            currentCell.next = newCell;
+        }
+        currentCell = currentCell.next;
     }
 }

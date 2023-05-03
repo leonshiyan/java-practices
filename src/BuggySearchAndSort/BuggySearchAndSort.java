0cad7c190b11292bfcd8fac11b02f350833e68f5
leonshiyan <leonshiyan@gmail.com>
2023-05-03 15:03:19 +0800

    bbs method bug fix

--- src/BuggySearchAndSort/BuggySearchAndSort.java
+++ src/BuggySearchAndSort/BuggySearchAndSort.java
@@ -70,15 +70,15 @@ public class BuggySearchAndSort {
 	 * to the length of the array.
 	 */
 	public static void bubbleSort(int[] array) {
-		for (int i = 0; i < array.length; i++) {
-			for (int j = 0; j < array.length-1; i++) {
-				if (array[j] > array[j+1]) { // swap elements j and j+1
-					int temp = array[j];
-					array[j] = array[j+1];
-					array[j+1] = temp;
-				}
-			}
-		}
+	    for (int i = 0; i < array.length; i++) {
+	        for (int j = 0; j < array.length-1; j++) {
+	            if (array[j] > array[j+1]) { // swap elements j and j+1
+	                int temp = array[j];
+	                array[j] = array[j+1];
+	                array[j+1] = temp;
+	            }
+	        }
+	    }
 	}
 	
 	/**

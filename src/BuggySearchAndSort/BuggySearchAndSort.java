d7caa476684d24876a3e01c99f9a371e3d5ccc2f
leonshiyan <leonshiyan@gmail.com>
2023-05-03 15:00:50 +0800

    method contains bug fix

--- src/BuggySearchAndSort/BuggySearchAndSort.java
+++ src/BuggySearchAndSort/BuggySearchAndSort.java
@@ -56,13 +56,11 @@ public class BuggySearchAndSort {
 	 * @return true if val is one of the items in the array, false if not
 	 */
 	public static boolean contains(int[] array, int val) {
-		for (int i = 0; i < array.length; i++) {
-			if (array[i] == val)
-				return true;
-			else
-				return false;
-		}
-		return false;
+	    for (int i = 0; i < array.length; i++) {
+	        if (array[i] == val)
+	            return true;
+	    }
+	    return false;
 	}
 	
 	/**

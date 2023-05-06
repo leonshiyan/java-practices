86eecfb321dbebae74bcc73ff9577c1e645d43a9
leonshiyan <leonshiyan@gmail.com>
2023-05-06 19:08:40 +0800

    file io and basic verification

--- src/SpellChecker/SpellChecker.java
+++ src/SpellChecker/SpellChecker.java
@@ -1,10 +1,36 @@
 package SpellChecker;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
 public class SpellChecker {
+    public static void main(String[] args) {
+        Set<String> dictionary = new HashSet<>();
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+        try {
+            // Read words from file and store them in the set
+        	Scanner filein = new Scanner(new File("src/SpellChecker/words.txt"));
+            while (filein.hasNext()) {
+                String word = filein.next().toLowerCase();
+                dictionary.add(word);
+            }
+            filein.close();
 
-	}
+            // Check the size of the set
+            System.out.println("Dictionary size: " + dictionary.size());
 
+            // Check if a word is in the dictionary
+            String wordToCheck = "hello";
+            if (dictionary.contains(wordToCheck.toLowerCase())) {
+                System.out.println(wordToCheck + " is spelled correctly.");
+            } else {
+                System.out.println(wordToCheck + " is misspelled.");
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found: " + e.getMessage());
+        }
+    }
 }

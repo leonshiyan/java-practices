e03aaaee56a8dca86c89bd5e3a15a340ed61d9d4
leonshiyan <leonshiyan@gmail.com>
2023-05-07 14:46:02 +0800

    add ability to select text file for word compare

--- src/SpellChecker/SpellChecker.java
+++ src/SpellChecker/SpellChecker.java
@@ -5,6 +5,7 @@ import java.io.FileNotFoundException;
 import java.util.HashSet;
 import java.util.Scanner;
 import java.util.Set;
+import javax.swing.JFileChooser;
 
 public class SpellChecker {
     public static void main(String[] args) {
@@ -12,7 +13,7 @@ public class SpellChecker {
 
         try {
             // Read words from file and store them in the set
-        	Scanner filein = new Scanner(new File("src/SpellChecker/words.txt"));
+            Scanner filein = new Scanner(new File("src/SpellChecker/words.txt"));
             while (filein.hasNext()) {
                 String word = filein.next().toLowerCase();
                 dictionary.add(word);
@@ -22,15 +23,41 @@ public class SpellChecker {
             // Check the size of the set
             System.out.println("Dictionary size: " + dictionary.size());
 
-            // Check if a word is in the dictionary
-            String wordToCheck = "hello";
-            if (dictionary.contains(wordToCheck.toLowerCase())) {
-                System.out.println(wordToCheck + " is spelled correctly.");
-            } else {
-                System.out.println(wordToCheck + " is misspelled.");
+            // Let the user select an input file
+            File inputFile = getInputFileNameFromUser();
+            if (inputFile == null) {
+                System.out.println("No file selected.");
+                return;
             }
+
+            // Read words from the selected file and check against the dictionary
+            Scanner inputFileScanner = new Scanner(inputFile);
+            inputFileScanner.useDelimiter("[^a-zA-Z]+");
+
+            while (inputFileScanner.hasNext()) {
+                String word = inputFileScanner.next().toLowerCase();
+                if (!dictionary.contains(word)) {
+                    System.out.println("Misspelled word: " + word);
+                }
+            }
+            inputFileScanner.close();
         } catch (FileNotFoundException e) {
             System.out.println("File not found: " + e.getMessage());
         }
     }
+
+    /**
+     * Lets the user select an input file using a standard file selection dialog box.
+     * If the user cancels the dialog without selecting a file, the return value is null.
+     */
+    static File getInputFileNameFromUser() {
+        JFileChooser fileDialog = new JFileChooser();
+        fileDialog.setDialogTitle("Select File for Input");
+        int option = fileDialog.showOpenDialog(null);
+        if (option != JFileChooser.APPROVE_OPTION)
+            return null;
+        else
+            return fileDialog.getSelectedFile();
+    }
 }
+

5b4b6f8fda8fb048bd4ba9c0c57a70840c100030
leonshiyan <leonshiyan@gmail.com>
2023-05-08 09:45:51 +0800

    add wording suggestion

--- src/SpellChecker/SpellChecker.java
+++ src/SpellChecker/SpellChecker.java
@@ -5,6 +5,7 @@ import java.io.FileNotFoundException;
 import java.util.HashSet;
 import java.util.Scanner;
 import java.util.Set;
+import java.util.TreeSet;
 import javax.swing.JFileChooser;
 
 public class SpellChecker {
@@ -34,13 +35,25 @@ public class SpellChecker {
             Scanner inputFileScanner = new Scanner(inputFile);
             inputFileScanner.useDelimiter("[^a-zA-Z]+");
 
+            Set<String> misspelledWords = new HashSet<>();
+
             while (inputFileScanner.hasNext()) {
                 String word = inputFileScanner.next().toLowerCase();
                 if (!dictionary.contains(word)) {
-                    System.out.println("Misspelled word: " + word);
+                    misspelledWords.add(word);
                 }
             }
             inputFileScanner.close();
+
+            // Generate corrections for misspelled words and print suggestions
+            for (String misspelledWord : misspelledWords) {
+                Set<String> suggestions = corrections(misspelledWord, dictionary);
+                if (suggestions.isEmpty()) {
+                    System.out.println(misspelledWord + ": (no suggestions)");
+                } else {
+                    System.out.println(misspelledWord + ": " + suggestions);
+                }
+            }
         } catch (FileNotFoundException e) {
             System.out.println("File not found: " + e.getMessage());
         }
@@ -59,5 +72,58 @@ public class SpellChecker {
         else
             return fileDialog.getSelectedFile();
     }
+
+    /**
+     * Generates a TreeSet<String> containing variations on the badWord that are contained in the dictionary.
+     */
+    static TreeSet<String> corrections(String badWord, Set<String> dictionary) {
+        TreeSet<String> suggestions = new TreeSet<>();
+
+        // Delete any one of the letters from the misspelled word
+        for (int i = 0; i < badWord.length(); i++) {
+            String correctedWord = badWord.substring(0, i) + badWord.substring(i + 1);
+            if (dictionary.contains(correctedWord)) {
+                suggestions.add(correctedWord);
+            }
+        }
+        for (int i = 0; i < badWord.length(); i++) {
+            for (char ch = 'a'; ch <= 'z'; ch++) {
+                String correctedWord = badWord.substring(0, i) + ch + badWord.substring(i + 1);
+                if (dictionary.contains(correctedWord)) {
+                    suggestions.add(correctedWord);
+                }
+            }
+        }
+
+        // Insert any letter at any point in the misspelled word
+        for (int i = 0; i <= badWord.length(); i++) {
+            for (char ch = 'a'; ch <= 'z'; ch++) {
+                String correctedWord = badWord.substring(0, i) + ch + badWord.substring(i);
+                if (dictionary.contains(correctedWord)) {
+                    suggestions.add(correctedWord);
+                }
+            }
+        }
+
+        // Swap any two neighboring characters in the misspelled word
+        for (int i = 0; i < badWord.length() - 1; i++) {
+            String correctedWord = badWord.substring(0, i) + badWord.charAt(i + 1) + badWord.charAt(i) + badWord.substring(i + 2);
+            if (dictionary.contains(correctedWord)) {
+                suggestions.add(correctedWord);
+            }
+        }
+
+        // Insert a space at any point in the misspelled word (and check that both of the resulting words are in the dictionary)
+        for (int i = 1; i < badWord.length(); i++) {
+            String word1 = badWord.substring(0, i);
+            String word2 = badWord.substring(i);
+            if (dictionary.contains(word1) && dictionary.contains(word2)) {
+                suggestions.add(word1 + " " + word2);
+            }
+        }
+
+        return suggestions;
+    }
 }
+
 
